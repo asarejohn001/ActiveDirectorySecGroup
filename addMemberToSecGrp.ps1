5da419f032f9e986d7abd776f3789bc6fe81233a
asarejohn001 <asarejohn001@gmail.com>
2024-08-02 17:42:20 -0400

    Add function to log message into a log file Add comments to remote connection code block

--- addMemberToSecGrp.ps1
+++ addMemberToSecGrp.ps1
@@ -5,24 +5,44 @@ Date: 7/30/24
 Description: Use below code to manage security group on active direocty
 #>
 
+# Fucntion to log output
+function Get-Log {
+    param (
+        [string]$LogFilePath,
+        [string]$LogMessage
+    )
+
+    # Create the log entry with the current date and time
+    $logEntry = "{0} - {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $LogMessage
+
+    # Append the log entry to the log file
+    Add-Content -Path $LogFilePath -Value $logEntry
+}
+
+# Define the log file path
+$logFilePath = "enter log file path"
+
+# Install Active Directory module, if not already installed
+Install-Module ActiveDirectory # not needed if machine have RSAT installed
+
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
 # Define the group name
-$groupName = "Enter the group name"
+$groupName = "enter group name"
 
 # Define the path to the CSV file
-$members = "enter path to the csv file"
+$members = "enter csv file"
 
 # Define the remote computer and credentials
-$remoteComputer = "RemoteADComputerName"
-$credential = Get-Credential
+$remoteComputer = "enter domain name" # not needed if running script from a computer on the domain
+$credential = Get-Credential # not needed if running script from a computer on the domain
 
 # Create a new PSSession
-$session = New-PSSession -ComputerName $remoteComputer -Credential $credential
+$session = New-PSSession -ComputerName $remoteComputer -Credential $credential # not needed if running script from a computer on the domain
 
 # Invoke the script block on the remote machine
-Invoke-Command -Session $session
+Invoke-Command -Session $session # not needed if running script from a computer on the domain
 
 # Get emails of the CSV file, use the email to retrieve the distinName from AD, and then add member to the sec group
 Import-Csv -Path $members | ForEach-Object {
@@ -31,24 +51,29 @@ Import-Csv -Path $members | ForEach-Object {
         $user = Get-ADUser -Filter "EmailAddress -eq '$email'"
         if ($user) {
             Add-ADGroupMember -Identity $groupName -Members $user.DistinguishedName -ErrorAction Stop
-            Write-Host "Successfully added $email to $groupName"
+            Get-Log -LogFilePath $logFilePath -LogMessage "Successfully adeed $($user.DistinguishedName) to $groupName"
+            Write-Host "Successfuly added users. Check the log file"
         } else {
-            Write-Host "User with email $email not found in Active Directory."
+            Get-Log -LogFilePath $logFilePath -LogMessage "User with email $email not found in Active Directory."
+            Write-Host "Users were not found, check log file"
         }
     } catch {
-        Write-Host "Failed to add $email to $groupName. Error: $_"
+        Get-Log -LogFilePath $logFilePath -LogMessage "Failed to add $($user.DistinguishedName) to $groupName. Error: $_"
+        Write-Host "Failed to add users, check log file"
     }
 }
 
 
 # Read the SamAccount from the CSV file and remove each member from the group
-Import-Csv -Path $membersToRemove | ForEach-Object {
-    $samAccountName = $_.samAccountName
+Import-Csv -Path $members | ForEach-Object {
+    $samAccountName = $_.SamAccountName
     try {
         Remove-ADGroupMember -Identity $groupName -Members $samAccountName -Confirm:$false -ErrorAction Stop
-        Write-Host "Successfully removed $samAccountName from $groupName"
+        Get-Log -LogFilePath $logFilePath -LogMessage "Successfully removed $samAccountName from $groupName"
+        Write-Host "Successfully removed users, check log file"
     } catch {
-        Write-Host "Failed to remove $samAccountName from $groupName. Error: $_"
+        Get-Log -LogMessage "Failed to remove $samAccountName from $groupName. Error: $_" -LogFilePath $logFilePath
+        Write-Host "Failed to removed users"
     }
 }
 
